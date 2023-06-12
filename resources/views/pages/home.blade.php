66d30ef22e504d8484dfee23f29417a70eb6b9c7
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-06-12 21:49:16 +0200

    Esercizio_completato

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,16 +3,16 @@
 @section('trains')
 <main>
     @foreach ($trains as $elem)
-    <div class="d-flex">
-        <div>Compagnia: {{ $elem['Azienda'] }}</div>
-        <div>Arrivo: {{ $elem['Stazione_di_arrivo'] }}</div>
-        <div>Partenza: {{ $elem['Stazione_di_partenza'] }}</div>
-        <div>Orario di partenza: {{ $elem['Orario_di_partenza'] }}</div>
-        <div>Orario di arrivo: {{ $elem['Orario_di_arrivo'] }}</div>
-        <div>Treno: {{ $elem['Codice_Treno'] }}</div>
-        <div>Carrozza nr. {{ $elem['Numero_Carrozze'] }}</div>
-        <div>Ritardo: {{ $elem['In_orario'] }}</div>
-        <div>Cancellato: {{ $elem['Cancellato'] }}</div>
+    <div id="container" class="d-flex">
+        <div class="riga d-flex justify-content-center align-items-center">Compagnia: {{ $elem['Azienda'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Arrivo: {{ $elem['Stazione_di_arrivo'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Partenza: {{ $elem['Stazione_di_partenza'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Orario di partenza: {{ $elem['Orario_di_partenza'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Orario di arrivo: {{ $elem['Orario_di_arrivo'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Treno: {{ $elem['Codice_Treno'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Carrozza nr. {{ $elem['Numero_Carrozze'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Ritardo: {{ $elem['In_orario'] }}</div>
+        <div class="riga d-flex justify-content-center align-items-center">Cancellato: {{ $elem['Cancellato'] }}</div>
     </div>
     @endforeach
 </main>
